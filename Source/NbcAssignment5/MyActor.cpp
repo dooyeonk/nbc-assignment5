06c60691c49e18b0b6938f5e9dede0e931db06d8
강두연 <dyhouse0517@gmail.com>
2025-12-26 00:00:51 +0900

    feat: 과제5 기능 구현

---
+++ Source/NbcAssignment5/MyActor.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyActor.h"
+
+#include "Engine/Engine.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+    MoveCount = 0;
+    StartLocation = FVector(0, 50, 0);
+    TotalMoveCount = 10;
+    EventCount = 0;
+    TotalDistance = 0;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+    Super::BeginPlay();
+
+    SetActorLocation(StartLocation);
+    GetWorldTimerManager().SetTimer(TimerHandle, this, &AMyActor::RandomMoveAndTurn, 0.5f, true);
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void AMyActor::Move(const FVector& NewLocation) {
+    TotalDistance += FVector::Distance(GetActorLocation(), NewLocation);
+    SetActorLocation(NewLocation);
+}
+
+void AMyActor::Turn(const FRotator& NewRotation) {
+    SetActorRotation(NewRotation);
+}
+
+void AMyActor::RandomMoveAndTurn() {
+    if (MoveCount++ < 10) {
+        FVector RandomLocation = FVector(
+            FMath::RandRange(-300.f, 300.f),
+            FMath::RandRange(-300.f, 300.f),
+            FMath::RandRange(0.f, 300.f)
+        );
+        FRotator RandomRotation = FRotator(
+            FMath::RandRange(-300.f, 300.f),
+            FMath::RandRange(-300.f, 300.f),
+            FMath::RandRange(-300.f, 300.f)
+        );
+
+        Move(RandomLocation);
+        Turn(RandomRotation);
+
+        SCREENLOG(FColor::Blue, TEXT("move count: %d"), MoveCount);
+        SCREENLOG(FColor::Blue, TEXT("location: %s"), *RandomLocation.ToString());
+        //SCREENLOG(FColor::Blue, TEXT("rotation: %s"), *NewRotation.ToString());
+
+        if (EventTriggered()) {
+            SCREENLOG(FColor::Green, TEXT("EVENT TRIGGERED"));
+        }
+    }
+    else {
+        SCREENLOG(FColor::Yellow, TEXT("total distance: %.2f, event triggered count: %d"), TotalDistance, EventCount);
+        GetWorldTimerManager().ClearTimer(TimerHandle);
+    }
+}
+
+bool AMyActor::EventTriggered() {
+    float RandomValue = FMath::FRand();
+    if (RandomValue >= 0.5f) {
+        EventCount++;
+        return true;
+    }
+
+    return false;
+}
---
+++ Source/NbcAssignment5/MyActor.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#define SCREENLOG(Color, Format, ...) \
+    if (GEngine) { GEngine->AddOnScreenDebugMessage(-1, 20.0f, Color, FString::Printf(Format, ##__VA_ARGS__)); }
+
+#include "MyActor.generated.h"
+
+UCLASS()
+class NBCASSIGNMENT5_API AMyActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    AMyActor();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    void Move(const FVector& NewLocation);
+    void Turn(const FRotator& NewRotation);
+    void RandomMoveAndTurn();
+
+    UPROPERTY(EditAnywhere, Category = "Settings")
+    FVector StartLocation;
+
+    UPROPERTY(EditAnywhere, Category = "Settings")
+    int32 TotalMoveCount;
+
+private:
+    int32 MoveCount;
+    FTimerHandle TimerHandle;
+    int32 EventCount;
+    double TotalDistance;
+
+    bool EventTriggered();
+};
